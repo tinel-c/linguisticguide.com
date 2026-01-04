63508bdc22a7d14859c2854b0635b0392c37bec7
Constantin Bogza <tinel.c@gmail.com>
2026-01-04 21:14:15 +0200

    Fix manufacturing image blocked by CSP and update image URL

--- public/contact-handler.php
+++ public/contact-handler.php
@@ -23,7 +23,7 @@ header('X-Frame-Options: SAMEORIGIN');
 header('X-Content-Type-Options: nosniff');
 header('X-XSS-Protection: 1; mode=block');
 header('Referrer-Policy: strict-origin-when-cross-origin');
-header('Content-Security-Policy: default-src \'self\'; script-src \'self\' https://www.google.com https://www.gstatic.com; frame-src https://www.google.com; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; font-src \'self\' https://fonts.gstatic.com;');
+header('Content-Security-Policy: default-src \'self\'; script-src \'self\' https://www.google.com https://www.gstatic.com; frame-src https://www.google.com; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; font-src \'self\' https://fonts.gstatic.com; img-src \'self\' data: https://images.unsplash.com;');
 
 // Configuration
 define('RECIPIENT_EMAIL', 'monica.vlad@linguisticguide.com');
